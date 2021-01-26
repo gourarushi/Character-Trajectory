922c7aefed650e5f3e8e13491859e20bd0f1fbf9
Arushi.Nimish Gour <gour@rhrk.uni-kl.de>
2021-01-26 23:08:35 +0100

    Replace network_f.py

--- project/common_modules/network_f.py
+++ project/common_modules/network_f.py
@@ -18,7 +18,7 @@ from tqdm.notebook import tqdm
 
 
 # train network
-def trainNet(net,criterion,optimizer,scheduler,train_loader,val_loader,epochs,print_every=None,earlyStopping=None):
+def trainNet(net,criterion,optimizer,scheduler,train_loader,val_loader,epochs,print_every=None,earlyStopping=None,approach="defailt"):
 
   # early stopping parameter indicates how long to wait after last time validation loss improved.
 
@@ -37,8 +37,12 @@ def trainNet(net,criterion,optimizer,scheduler,train_loader,val_loader,epochs,pr
     net.train()
     for i, (inputBatch,labelBatch) in enumerate(train_loader):
 
-        inputBatch, labelBatch = inputBatch.to(device), labelBatch.to(device)
-        inputBatch = inputBatch.float()
+        if approach == "fusion":
+          inputBatch = [ip.to(device).float() for ip in inputBatch]
+        else:
+          inputBatch = inputBatch.to(device).float()
+
+        labelBatch = labelBatch.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -56,8 +60,10 @@ def trainNet(net,criterion,optimizer,scheduler,train_loader,val_loader,epochs,pr
     for i, (inputBatch,labelBatch) in enumerate(val_loader):
       with torch.no_grad():
 
-        inputBatch, labelBatch = inputBatch.to(device), labelBatch.to(device)
-        inputBatch = inputBatch.float()
+        if approach == "fusion":
+          inputBatch = [ip.to(device).float() for ip in inputBatch]
+        else:
+          inputBatch = inputBatch.to(device).float()
 
         # forward + backward + optimize
         outputBatch = net(inputBatch)
@@ -96,9 +102,9 @@ def trainNet(net,criterion,optimizer,scheduler,train_loader,val_loader,epochs,pr
 
 
 # evaluate and print
-def evaluate(net,input1,output_true,classes=None):
+def evaluate(net,input1,output_true,classes=None, approach="default"):
 
-  output_pred = netOutput(net,input1,type="class")
+  output_pred = netOutput(net,input1,outType="class", approach=approach)
 
   if classes is not None:
     print(classification_report(output_true, output_pred, target_names=classes, labels=range(len(classes)) ,digits=4))
@@ -106,19 +112,25 @@ def evaluate(net,input1,output_true,classes=None):
     print(classification_report(output_true, output_pred, digits=4))
 
 
-def netOutput(net, inputs, type="logits"):
+def netOutput(net, inputs, outType="default", approach="default"):
   net.eval()
   outputs = []
 
   with torch.no_grad():
     for input1 in tqdm(inputs):
-      if type == "latent":
+      if outType == "latent":
         output = net.latent(torch.from_numpy(input1).unsqueeze(0).float()).numpy()
-      elif type == "class":
-        output = net(torch.from_numpy(input1).unsqueeze(0).float()).numpy()
+
+      elif outType == "class":
+        if approach == "fusion":
+          input1 = [torch.from_numpy(ip).unsqueeze(0).float() for ip in input1]
+          output = net(input1).numpy()
+        else:  
+          output = net(torch.from_numpy(input1).unsqueeze(0).float()).numpy()
         output = np.argmax(output)
+
       else:
-         output = net(torch.from_numpy(input1).unsqueeze(0).float()).numpy()
+        output = net(torch.from_numpy(input1).unsqueeze(0).float()).numpy()
 
       outputs.append(output)
 
